12d5331bf7f91b2d8da5d5bd907f46b0cbdd78ae
LAPTOP-2R574CEK\USER <sgjang@dhflour.co.kr>
2025-04-10 09:24:28 +0900

    logo, 문의전화번호, height 수정

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -6,17 +6,17 @@ import logo from '@/assets/images/icon_logo_df_w.svg';
 
 export const Footer = () => {
   return (
-      <footer className="footer py-4">
-        <div className="container mx-auto md:px-6">
+      <footer className="footer">
+        <div className="container">
           <div className="flex items-center justify-between">
             <div className="logo">
-              <Link to="/" className="block w-fit">
-                <img src={logo} alt="DHFLOUR" className="h-10" />
+              <Link to="/">
+                <img src={logo} alt="DHFLOUR"/>
               </Link>
             </div>
             <div className="ft-left">
               <div className="info">
-                <p className="whitespace-nowrap text-white text-sm">
+                <p className="whitespace-nowrap text-white text-m">
                   문의전화번호 :
                   <Link to={`tel:02-3455-0000`} className="text-white"> 02-3455-0000</Link>
                 </p>
